3d3041e78964353d01ace7be4592dbf838d38496
Wilson <wheres67@gmail.com>
2023-03-26 21:41:39 -0400

    adding logic/UI for getting and adding posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,30 @@ import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 import { api } from "~/utils/api";
 
+const CreatePostWizard = () => {
+
+  const { user } = useUser();
+
+  if (!user) return null;
+
+
+  return <div className="flex gap-3 w-full">
+    <img src={user.profileImageUrl} alt="Profile Image" className="w-14 h-14 rounded-full" />
+    <input placeholder="Type some emojis!" className="bg-transparent grow outline-none" />
+  </div>
+};
+
+
+
 const Home: NextPage = () => {
 
   const user = useUser();
 
   const { data, isLoading } = api.posts.getAll.useQuery();
 
-  if(isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div>Loading...</div>;
 
-  if(!data) return <div>Something went wrong</div>;
+  if (!data) return <div>Something went wrong</div>;
 
   return (
     <>
@@ -26,10 +41,12 @@ const Home: NextPage = () => {
         <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
           <div className="flex border-b border-slate-400 p-4 ">
             {!user.isSignedIn && <div className="flex justify-center"><SignInButton /></div>}
-            {!!user.isSignedIn && <SignOutButton />}
+            {user.isSignedIn && <CreatePostWizard />}
           </div>
           <div className="flex flex-col">
-            {data?.map((post) => (<div key={post.id} className="p-8 border-b border-slate-400">{post.content}</div>))}
+            {[...data]?.map(({ post, author }) => (<div key={post.id} className="p-8 border-b border-slate-400">
+              {post.content}
+            </div>))}
           </div>
         </div>
       </main>
